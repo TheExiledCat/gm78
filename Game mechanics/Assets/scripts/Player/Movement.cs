8d8a16a6464f7277fa5e4d1b5f92ce0cfa18e265
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2019-04-02 16:19:50 +0200

    Main game

---
+++ Game mechanics/Assets/scripts/Player/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    public GameObject goGun;
+    float fMoveInX;
+    float fMoveInY;
+    float ms = 10;
+    float fLookAngle;
+    bool bHasGun=false;
+    bool bIsSuper = false;
+    bool bIsAlive = false;
+    Vector2 v2MousePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bIsAlive = true;//set player to be alive
+        rb = GetComponent<Rigidbody>();//get ridgidbody into rb var
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        v2MousePos = Input.mousePosition;
+        goGun.transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);//rotate gun around player
+        fMoveInX = Input.GetAxis("Horizontal");//Input
+        fMoveInY = Input.GetAxis("Vertical");//Input
+        rb.velocity = new Vector3(fMoveInX * ms, 0, fMoveInY*ms);//movement oneliner
+        Debug.Log(v2MousePos);
+    }
+}
