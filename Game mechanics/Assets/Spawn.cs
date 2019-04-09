689af6a8d3bc90b84d0ea8d2fe3287aac0279b11
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2019-04-09 14:12:00 +0200

    spawner made

---
+++ Game mechanics/Assets/Spawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    int wave = 1;
+    GameObject creeper;
+    
+    public List<GameObject> creepers = new List<GameObject>();
+    void Spawner()
+    {
+        for(int i = 0; i < wave; i++)
+        {
+            var creep =Instantiate(creeper, new Vector3(transform.position.x + i * 0.1f, GameObject.Find("Player").transform.position.y, transform.position.z), Quaternion.identity, gameObject.transform);
+            creepers.Add(creep);
+        }
+    }
+    private void Start()
+    {
+        creeper = Resources.Load<GameObject>("DJRCreeper");
+        Spawner();
+    }
+    private void Update()
+    {
+        
+    }
+    private void NextWave()
+    {
+        wave++;
+        Spawner();
+    }
+}
--- Game mechanics/Assets/scripts/enemies/Death.cs
+++ Game mechanics/Assets/scripts/enemies/Death.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Death : MonoBehaviour
 {
     GameObject player;
+    
     [HideInInspector]
     public int hp;
     // Start is called before the first frame update
     void Start()
     {
+        
         player = Resources.Load<GameObject>("Player");
         hp =  100;
         
