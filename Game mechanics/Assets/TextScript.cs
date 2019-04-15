eee09b3969a13c5ea593a80b451e9c89d0683308
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2019-04-15 11:40:06 +0200

    added basic ui

---
+++ Game mechanics/Assets/TextScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TextScript : MonoBehaviour
+{
+    GameObject spn;
+    TextMeshProUGUI t;
+    private void Start()
+    {
+        spn = GameObject.Find("Spawner");
+        t = GameObject.Find("Wave").GetComponent<TextMeshProUGUI>();
+    }
+    private void Update()
+    {
+        t.text = spn.GetComponent<Spawn>().wave.ToString("000");
+    }
+}
