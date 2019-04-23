2a57fb042a7abdb7650fbf6208a0fbfd51a42a8c
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2019-04-23 14:57:20 +0200

    added clock and death

--- Game mechanics/Assets/scripts/Player/Movement.cs
+++ Game mechanics/Assets/scripts/Player/Movement.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class Movement : MonoBehaviour
 {
     MouseToWorld mtw;
@@ -13,32 +14,37 @@ public class Movement : MonoBehaviour
     float fMoveInY;
     float ms = 10;
     float fLookAngle;
-    
+    public GameObject clock;
     public bool bIsSuper = false;
     bool bIsAlive = false;
     int ammo = 5;
-    
+    int supertime;
+
     // Start is called before the first frame update
     void Start()
     {
         mr = GetComponent<MeshRenderer>();
         mtw = GetComponent<MouseToWorld>();
-        bIsAlive = true;//set player to be alive
+        
         rb = GetComponent<Rigidbody>();//get ridgidbody into rb var
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
-        
-        
 
+        Debug.Log(supertime);
+
+        if (supertime > 0)
+        {
+            supertime--;
+        }
+        Debug.Log(clock.GetComponent<Image>().fillAmount);
+        clock.GetComponent<Image>().fillAmount = (float)supertime / 300 ;
 
         fMoveInX = Input.GetAxis("Horizontal");//Input
         fMoveInY = Input.GetAxis("Vertical");//Input
-        rb.velocity = new Vector3(fMoveInX * ms, 0, fMoveInY*ms);//movement oneliner
+        rb.velocity = new Vector3(fMoveInX * ms, 0, fMoveInY * ms);//movement oneliner
         if (bIsSuper)
         {
             mr.material = super;
@@ -47,28 +53,33 @@ public class Movement : MonoBehaviour
         {
             mr.material = normal;
         }
-        
-        
+
+
     }
     private void FixedUpdate()
     {
         float dist = Vector3.Distance(transform.position, mtw.point);
-        if(dist >= 1f)
+        if (dist >= 1f)
         {
             transform.LookAt(mtw.point);
         }
         transform.rotation = new Quaternion(0, transform.rotation.y, 0, transform.rotation.w);
-        
-        
+
+
     }
     private void OnTriggerEnter(Collider other)
     {
-        
+
         if (other.CompareTag("item"))
         {
             Destroy(other.gameObject);
             bIsSuper = true;
             Invoke("SuperSwap", 5f);
+            supertime = 300;
+        }
+        if (other.CompareTag("enemy"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
     void SuperSwap()
